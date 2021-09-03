f1391a5134cef080ba7d5351de7f99193d91a095
Alice <andreevaalisa42@gmail.com>
2021-09-03 15:03:54 +0300

    add clickHandler

--- src/components/RandomAdvice/RandomAdvice.js
+++ src/components/RandomAdvice/RandomAdvice.js
@@ -3,6 +3,14 @@ import styles from './randomadvice.module.css';
 
 function RandomAdvice() {
   let [advice, setAdvice] = useState(null);
+
+  function clickHandler(e) {
+    e.preventDefault();
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(data => data.slip)
+    .then(data => setAdvice(data.advice));
+  }
   
 
   useEffect(() => {
@@ -18,6 +26,7 @@ function RandomAdvice() {
     <div className={styles.advice}> 
       {advice ? <div>{advice}</div> : 'loading...'}
     </div><br/>
+    <button onClick={clickHandler}>Another Advice</button>
     </>
   );
 }
